f0db82e128f1fbd17b0600dbcf4b5fce1f93ebf1
Tassa Tenhouse <kmtenhouse@gmail.com>
2018-11-26 00:31:44 -0800

    Added ability to use dotenv for environment variables with our current setup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,18 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  //Commenting out this boilerplate for now so we can test the static routes
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  
+/*   app.get("/", function(req, res) {
+
+     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
       });
-    });
-  });
+    }); 
+  }); */
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
