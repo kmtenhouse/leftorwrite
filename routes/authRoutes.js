4db4c4e94fc874eb0719da55190185920307cd94
J.Lee <joverlee521@gmail.com>
2018-11-27 13:54:37 -0800

    added logout button

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ var passport = require("passport");
 var db = require("../models");
 
 module.exports = function(app){
+    // Loads cookie session status
     app.get("/cookies", function(req, res){
         if(req.session.token){
             res.cookie("token", req.session.token);
@@ -17,14 +18,17 @@ module.exports = function(app){
         }
     });
 
+    // Redirects to Google Sign in Page
     app.get("/auth/google", 
         passport.authenticate("google", { scope: ["https://www.googleapis.com/auth/plus.login"] }));
     
+    // Callback URL after sign-in 
     app.get("/auth/google/callback", 
         passport.authenticate("google", {failureRedirect: "/login"}),
         function(req, res) {
             req.session.token = req.user.token;
             var user = req.user.profile;
+            // Finds or creates a new user with the user token id
             db.User.findOrCreate({
                 where: {
                     oAuthKey: user.id
@@ -33,16 +37,19 @@ module.exports = function(app){
                     displayName: user.displayName
                 }
             }).spread(function(user, created){
+                // If a new user was created, should redirect to a create user's page
                 if(created){
                     res.send("User Created");
                 }
+                // If user already exists, redirect to their page
                 else{
                     res.redirect("/");
                 }
             });
         }
     );
-
+    
+    // Logout URL
     app.get("/logout", function(req, res){
         req.logout();
         req.session = null;
