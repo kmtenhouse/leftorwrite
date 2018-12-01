8ff564d9654f96ac6bb6e0aa44f10b4a781ee62c
Kona Tess <konatess@gmail.com>
2018-12-01 10:56:52 -0800

    Just copied over the api routes file with the code from master on github, since my files were for some reason being difficult.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,17 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    // // Get all examples
-    // app.get("/api/examples", function (req, res) {
-    //     db.Example.findAll({}).then(function (dbExamples) {
-    //         res.json(dbExamples);
-    //     });
-    // });
+    // Get all examples
+    app.get("/api/examples", function (req, res) {
+        db.Example.findAll({}).then(function (dbExamples) {
+            res.json(dbExamples);
+        });
+    });
 
-    // // Create a new example
-    // app.post("/api/examples", function (req, res) {
-    //     db.Example.create(req.body).then(function (dbExample) {
-    //         console.log(req.body);
-    //         // res.json(dbExample);
-    //     });
-    // });
-
-    app.post("/story/edit/newtag", function (req, res) {
-        db.Tags.create(req.body).then(function(dbTags) {
-            res.json(dbTags);
+    // Create a new example
+    app.post("/api/examples", function (req, res) {
+        db.Example.create(req.body).then(function (dbExample) {
+            res.json(dbExample);
         });
     });
 
