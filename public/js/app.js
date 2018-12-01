ecc14a76ed516db4078007a0e8bbad945c838ccc
J.Lee <joverlee521@gmail.com>
2018-11-30 16:10:15 -0800

    added regex to check username

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,34 @@
+var usernameRegex = /^(?!_\h)(?!.*__)(?!.*\h{2})[A-Za-z0-9_\h]{3,50}(?<!_)$/;
+
 $("#newUsernameForm").on("submit", function(event){
     event.preventDefault();
     var newUsername = $("#username-input").val().trim();
-    $.get("/api/user/" + newUsername).then(function(data, status){
-        if(data.length === 0 && status === "success"){
-            var newUser = {
-                username: newUsername
-            };
-            $.ajax("/api/user", {
-                type: "PUT",
-                data: newUser
-            }).then(function(){
-                $("#newUsername").html(newUsername);
-                $("#updateUsernameModal").modal("show");
-                console.log("changed username!");
-            });
-        }
-        else{
-            $("#errorModal").modal("show");
-            console.log("Username already exists!");
-        }
-    });
-    
+    if(usernameRegex.test(newUsername)){
+        $.get("/api/user/" + newUsername).then(function(data, status){
+            if(data.length === 0 && status === "success"){
+                var newUser = {
+                    username: newUsername
+                };
+                $.ajax("/api/user", {
+                    type: "PUT",
+                    data: newUser
+                }).then(function(){
+                    $("#newUsername").html(newUsername);
+                    $("#updateUsernameModal").modal("show");
+                    console.log("changed username!");
+                });
+            }
+            else{
+                $("#errorModal").modal("show");
+                console.log("Username already exists!");
+            }
+        });
+    }
+    else{
+        $("#username-input").popover({
+            content: "Username must be 3-50 characters with only letters, numbers, underscores and spaces. Underscores and spaces cannot repeat.",
+            placement: "bottom"
+        });
+        $("#username-input").popover("show");
+    }
 });
