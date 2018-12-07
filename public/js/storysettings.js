34c31d3d7e4fef7c06f72906533c3a6ef5c0c5d7
Tassa Tenhouse <kmtenhouse@gmail.com>
2018-12-06 21:41:23 -0800

    Fixed a bug on the story settings page

--- public/js/storysettings.js
+++ public/js/storysettings.js
@@ -13,7 +13,7 @@ var tagAlreadyExists = "That tag already exists. If you can't find it using the 
 // test string from input against regex returns true if string is invalid
 function badInputTest(string, min, max) {
     var containsSpecial = specialCharTest.test(string);
-    if (string.length < min|| string.length > max || containsSpecial === true) {
+    if (string.length < min || string.length > max || containsSpecial === true) {
         return true;
     }
 }
@@ -28,12 +28,11 @@ function openPopover(location, message) {
     $(location).popover("show");
 }
 
-
-$(document).ready(function() {
+$(document).ready(function () {
     var retVal;
     // make tags loaded with class checked actually checked
     $(".checked").prop("checked", true);
-    $(".checked").each(function() {
+    $(".checked").each(function () {
         // if tag is checked
         if ($(this).hasClass("checked")) {
             var tag = $(this).data("tag-name");
@@ -70,32 +69,43 @@ function filterTags() {
         }
     }
 }
+
+//GLOBAL VARIABLES
+//Locally store our tag variables so that we don't iterate over them all each time something is checked or unchecked ;)
+var tagsArr = [];
+//Helper function to display the currently selected tags to the user
+function updateTagDisplay(newTag, checkedStatus) {
+    // if it's a brand new tag (recently clicked as true)
+    if (checkedStatus === true) {
+        tagsArr.push(newTag);
+        tagsArr.sort();
+        $("#chosenTags").val(tagsArr.join(", "));
+    }
+    // if tag is not checked, see if it's in our current tag array -- and remove it!
+    else if (checkedStatus === false) {
+        //find it in the array:
+        var currentIndex = tagsArr.indexOf(newTag);
+        if (currentIndex >= 0) { //juuuust sanity checking that the index value is real ;)
+            tagsArr.splice(currentIndex, 1);
+            // Populate to chosen tags field
+            $("#chosenTags").val(tagsArr.join(", "));
+        }
+    }
+
+}
+
 // Call filterTags function when anything is typed in the tag searchbar
 $(document).on("keyup", "#tagSearch", filterTags);
 
-// Declare tags array variable for sorting purposes.
-var tagsArr = [];
 // Display checked tags in chosen tags field.
 $(document).on("click", ".tag", function () {
+    //first, grab the name of this particular tag
     var tag = $(this).data("tag-name");
-    var retVal = "";
-    // if tag is checked
-    if ($(this).prop("checked") === true) {
-        tagsArr.push(tag);
-        tagsArr.sort();
-        retVal = tagsArr.join(", ");
-    }
-    // if tag is not checked
-    else if ($(this).prop("checked") === false) {
-        for (var i = 0; i < tagsArr.length; i++) {
-            if (tagsArr[i] === tag) {
-                tagsArr.splice(i, 1);
-                retVal = tagsArr.join(", ");
-            }
-        }
-    }
-    // Populate to chosen tags field
-    $("#chosenTags").val(retVal);
+    //next, grab the checked/unchecked status
+    //will be TRUE if checked, FALSE if not checked
+    var newCheckedStatus = ($(this).prop("checked"));
+    //finally, update the tag display!
+    updateTagDisplay(tag, newCheckedStatus);
 });
 // Populate searchbar input to input field on tag creation modal
 $(document).on("click", "#tagModalButton", function () {
@@ -116,7 +126,7 @@ $(document).on("click", "#saveChanges", function (event) {
     event.preventDefault();
     var id = $("#storyTitle").data("id");
     var title = $("#storyTitle").val().trim();
-    if (title !== "") { 
+    if (title !== "") {
         if (badInputTest(title, 2, 100)) {
             $(window).scrollTop(0);
             return openPopover("#storyTitle", titleMustBe);
@@ -124,27 +134,27 @@ $(document).on("click", "#saveChanges", function (event) {
     }
     var warns = {};
     var storytags = [];
-    $(".content-warning").each(function() {
+    $(".content-warning").each(function () {
         var status = $(this).hasClass("active");
         var id = $(this).attr("id");
         warns[id] = status;
     });
-    $(".tag").each(function() {
+    $(".tag").each(function () {
         if ($(this).prop("checked") === true) {
-            var tag =  $(this).data("tag-id");
+            var tag = $(this).data("tag-id");
             storytags.push(tag);
         }
     });
     var storyObj = {
         title: $("#storyTitle").val().trim(),
-        chooseNotToWarn: warns.chooseNotToWarn ,
+        chooseNotToWarn: warns.chooseNotToWarn,
         violence: warns.violence,
         nsfw: warns.nsfw,
         nonConsent: warns.nonConsent,
         characterDeath: warns.characterDeath,
         profanity: warns.profanity,
         tags: storytags.toString()
-    }; 
+    };
     // CREATE
     if (id === "") {
         $.ajax("/api/story/create/", {
@@ -159,7 +169,7 @@ $(document).on("click", "#saveChanges", function (event) {
         });
     }
     // UPDATE
-    else{
+    else {
         $.ajax("/api/story/update/" + id, {
             type: "PUT",
             data: storyObj
@@ -170,7 +180,7 @@ $(document).on("click", "#saveChanges", function (event) {
 });
 
 // CREATE ROUTE FOR TAGS
-$(document).on("click", "#saveTag", function() {
+$(document).on("click", "#saveTag", function () {
     event.preventDefault();
     var newtag = $("#newTagInput").val().trim();
     if (badInputTest(newtag, 2, 50)) {
@@ -182,7 +192,7 @@ $(document).on("click", "#saveTag", function() {
             data: {
                 tagName: newtag
             },
-            error(xhr,status,error) {
+            error(xhr, status, error) {
                 if (error === "Conflict") {
                     openPopover("#newTagInput", tagAlreadyExists);
                     // Populate modal input field value back to searchbar on save, 
@@ -192,23 +202,29 @@ $(document).on("click", "#saveTag", function() {
                 }
             }
         }).then(function (result, status) {
-            if (status==="success") {
+            //if we succeeded in adding the tag, we'll add it to the tag list, check it, and close the modal
+            if (status === "success") {
                 console.log("success");
                 console.log(result);
             }
             // add the new tag to the list without refreshing the page
-            var successfulTag = "<li class=\"list-group-item\">" + 
-            "<div class=\"form-check\">" +
-            "<input type=\"checkbox\" class=\"form-check-input tag\" id=\"tag"+ result.id + 
-            "\" data-tag-name=\""+ result.tagName + "\" data-tag-id=\""+ result.id +"\">" +
-            "<label class=\"form-check-label\" for=\"tag"+ result.id + "\">" + result.tagName +"</label>" +
-            "</div></li>";
-            successfulTag.prop("checked", true);
-            $("#tagList").children().last().before(successfulTag);
+            var successfulTagListItem = $("<li class=\"list-group-item\">");
+            var successfulTagHolder = $("<div class=\"form-check\">");
+            var successfulTagLabel = $("<label class=\"form-check-label\" for=\"tag" + result.id + "\">" + result.tagName + "</label>");
+            var successfulTagCheckBox = $("<input type=\"checkbox\" class=\"form-check-input tag\" id=\"tag" + result.id +
+                "\" data-tag-name=\"" + result.tagName + "\" data-tag-id=\"" + result.id + "\">");
+            successfulTagCheckBox.prop("checked", true);
+            successfulTagHolder.append(successfulTagCheckBox);
+            successfulTagHolder.append(successfulTagLabel);
+            successfulTagListItem.append(successfulTagHolder);
+            $("#tagList").children().last().before(successfulTagListItem);
             // Populate new tag value back to searchbar on save, 
             // and call filter function again
             $("#tagSearch").val(newtag);
+            //narrow the filter down to showcase this new tag
             filterTags();
+            //also show that we're including it 
+            updateTagDisplay(result.tagName, true);
             $("#createTagModal").modal("hide");
         });
     }
@@ -228,7 +244,7 @@ $(document).on("click", "#deleteStory", function () {
         });
     }
     // if no id, not created yet, therefore no delete. add error
-    else{
+    else {
         $.ajax("*", {
             type: "GET"
         });
@@ -238,14 +254,14 @@ $(document).on("click", "#deleteStory", function () {
 // OTHER BUTTONS 
 // links to other pages 
 // view story
-$(document).on("click", "#viewStory", function(event) {
+$(document).on("click", "#viewStory", function (event) {
     event.preventDefault();
     var storyId = $("#storyTitle").data("id");
     var url = "/story/overview/" + storyId;
     window.location = url;
 });
 // create new page
-$(document).on("click", "#createNewPage", function(event){
+$(document).on("click", "#createNewPage", function (event) {
     event.preventDefault();
     var storyId = $("#storyTitle").data("id");
     var url = "/story/write/" + storyId + "/pages";
